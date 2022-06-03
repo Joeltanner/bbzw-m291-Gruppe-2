dfb519b8ac19c8c138058a880a27dcad1533f357
Joeltanner <79570025+Joeltanner@users.noreply.github.com>
2022-06-03 15:35:36 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+const firstname = document.getElementById("firstname")
+const lastname = document.getElementById("lastname")
+const email = document.getElementById("email")
+const submit = document.getElementById("senden")
+
+
+submit.addEventListener("click", () => {
+console.log(firstname.value, lastname.value, email.value)
+})
+
+function checkFunction() {
+
+if (firstname.value == "" || lastname.value == "" || email.value == "") {
+submit.disabled = true;
+} else {
+submit.disabled = false;
+}
+}
+
+submit.addEventListener("click", async (event) => {
+    event.preventDefault()
+    const result = await databaseClient.insertInto("users", ["email", "firstname", "lastname"], [email.value, firstname.value, lastname.value] )
+    if (result.error) {
+        alert("Datenbank Fehler: " + JSON.stringify(result.error, null, 2))
+    }
+    else {
+        // Weiterleitung auf die Game Page  
+        location.href = "index.html"
+    
+    }})
+
